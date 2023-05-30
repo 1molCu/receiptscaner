d00fab2c23720c4650086e61192fb3a9496d3560
zengjt <jint@zengjt.com>
2023-05-31 00:31:43 +0800

    1

--- lib/units/value.dart
+++ lib/units/value.dart
@@ -1,14 +1,14 @@
-const String BaseUrl = 'https://xxxx.xxxxx.cn/api/receipt/';
+const String BaseUrl = 'https://wecan.onlyforu.cn/api/receipt/';
 
 const String InfoUrl = BaseUrl + 'info/';
 
 const String DownloadUrl = BaseUrl + 'download/';
 
-const String UploadUrl = 'https://xxxx.onlyfoxxxru.cn/upload';
+const String UploadUrl = 'https://wecan.onlyforu.cn/upload';
 
 const String regExUrl = r'zzsfp.xj-n-tax.gov.cn';
 
 const String urlPattern = r'\d+_\d+_\d+_[a-zA-Z0-9]+';
 
 const String appUpdateUrl =
-    'https://xxx.xxxxx.cn/api/receipt/appupdateinfo';
+    'https://wecan.onlyforu.cn/api/receipt/appupdateinfo';
